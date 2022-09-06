af8579de4037c8245b676ca665fd26a3f625e9f8
SeanRBurton <burton.seanr@gmail.com>
2022-09-06 10:57:50 +0100

    refine download_images

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -2,39 +2,43 @@
 # coding: utf-8
 
 import ee
-import geemap
 from ee import ImageCollection
-
-import os
+import geemap
 import numpy as np
-import matplotlib.pyplot as plt
+import pickle
 
-
-def download_map(county_name):
-    ee.Authenticate()
+def download_map(country_code, state_name, modis_collection='006/MOD13A1', num_pixels=5000):
     ee.Initialize()
     Map = geemap.Map()
-    counties_shp = '../raw_data/gadm41_SSD_shp/gadm41_SSD_2.shp'
+    counties_shp = f'../raw_data/gadm41_{country_code}_shp/gadm41_{country_code}_2.shp'
     ee_shape = geemap.shp_to_ee(counties_shp)
-    ee_shape.getInfo()
-    county_ee = ee_shape.filter(ee.Filter.eq("NAME_2", county_name))
-    Map.addLayer(county_ee.geometry())
+    state_ee = ee_shape.filter(ee.Filter.eq("NAME_2", state_name))
+    Map.addLayer(state_ee.geometry())
 
-    start_date = '2022-04-20'
-    end_date = '2022-05-04'
-    MODIS_layer = 'MOD13A1'  #(high to low resolution: MOD13Q1 MOD13A1 MOD13A2)
-    image_collection = ee.ImageCollection(f'MODIS/006/{MODIS_layer}').filter(ee.Filter.date(start_date,end_date)).select(['NDVI']).toBands()
+    start_date = '2021-01-01'
+    end_date = '2022-12-30'
+    image_collection = ee.ImageCollection(f'MODIS/{modis_collection}').filter(ee.Filter.date(start_date,end_date)).toBands()
 
-    region = county_ee.geometry()
+    region = state_ee.geometry()
 
-    sampled_pixels  = image_collection.sample(region, scale=300 , numPixels=5000)
+    sampled_pixels = [
+        sample['properties'] for sample in image_collection.sample(region, scale=300 , numPixels=num_pixels).getInfo()['features']
+    ]
 
-    pixel_values = sampled_pixels.getInfo()['features'][3]['properties']['2022_04_23_NDVI']
-    print(pixel_values)
+    return {k[11:] : np.array([sample[k] for sample in sampled_pixels]) for k in sampled_pixels[0]}
 
 test_name = 'Bahr al Jabal'
+results = download_map('SSD', test_name, num_pixels=5000)
+for k, v in results.items():
+    print(k, v)
 
-download_map(test_name)
+import matplotlib.pyplot as plt
+
+for i, (k, v) in enumerate(results.items()):
+    plt.subplot(6, 2, i + 1)
+    plt.title(k)
+    plt.hist(v, bins='auto')
+plt.show()
 
 #image_name = '2022_04_23_NDVI'
 #pixel_value_2022_04_23_NDVI_list = []
