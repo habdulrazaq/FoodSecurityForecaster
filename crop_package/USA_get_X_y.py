b36c82a5f34a9ea099335dc56fb99b2046759d54
SeanRBurton <burton.seanr@gmail.com>
2022-09-08 17:31:59 +0100

    change USA_get_X_y to exclude Florida

--- crop_package/USA_get_X_y.py
+++ crop_package/USA_get_X_y.py
@@ -6,6 +6,7 @@ def get_X_y(country_code='USA'):
     # 0. import df
     X_data = np.load(f'../data/USA_data.npz')
     X = X_data['X']
+    print("X shape:", X.shape)
 
     df_y = pd.read_csv(f'../raw_data/Crop yield data/COUNTY_level_annual/soybeans_usa.csv')
 
@@ -13,11 +14,10 @@ def get_X_y(country_code='USA'):
 
     state_names = [name.lower() for name in X_data['county_names']]
 
-    X_counties = set(state_names)
+    X_counties = set(state_names) - {'florida'}
     y_counties = set(df_y['STATE'])
 
-
-    X_in_y = np.array([name in y_counties for name in state_names])
+    X_in_y = np.array([name in y_counties and name != 'florida' for name in state_names])
 
     X = X[:,X_in_y]
 
@@ -25,10 +25,10 @@ def get_X_y(country_code='USA'):
                       .sort_values('STATE') \
                       .groupby('YEAR')
 
-
-
     y = np.zeros(X.shape[:2])
     for i, (year, group) in enumerate(year_groups):
+        if year == 2022:
+            break
         y[i] = group['YIELD']
 
 
