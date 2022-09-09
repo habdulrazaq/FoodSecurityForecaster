e85fd31cd6bad852c33f17340e4aed4b0e80d3f7
Thomas Johnson <johnsonthomas467@gmail.com>
2022-09-09 17:18:28 +0100

    deleted folder containing corrupted files

--- crop_package/download_images_other_countries.py
+++ crop_package/download_images_other_countries.py
@@ -66,7 +66,7 @@ def load_all(country_code='SSD', date_range=('2000-01-01', '2015-01-01'), modis_
     lower_case_country_code = country_code.lower
     states_shp = f'../raw_data/USA/admin1/USA.shp'
 
-    for state_name in state_names[0:]:
+    for state_name in state_names[33:]:
 
         list_df = []
         year_lst = [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2017,2018,2019,2020,2021]
