502ca8382363b571ceeefa95a6091a272eafa91a
Thomas Johnson <johnsonthomas467@gmail.com>
2022-09-09 12:55:55 +0100

    revised USA fit, get_X_y model, download images, added Alabama data

--- crop_package/download_images_other_countries.py
+++ crop_package/download_images_other_countries.py
@@ -19,7 +19,7 @@ import pandas as pd
 state_names = pd.read_csv('../raw_data/state_names_USA.csv')['NAME1_,C,33']
 
 # MYD11A2 (2002-2022) 8-day Aqua Land Surface Temperature and Emissivity 1km
-# MOD09Q1 (8-day surface spectral reflectance of bands 1 and 2 at 250m resolution)
+# MOD09A1 (8-day surface spectral reflectance of bands 1 and 2 at 500m resolution)
 # https://developers.google.com/earth-engine/datasets/catalog/MODIS_006_MCD12Q1
 def download_map(country_code,
                  state_name,
@@ -45,12 +45,12 @@ def download_map(country_code,
     samples = image_collection.sample(region, scale=250 , numPixels=num_pixels)
 
     features = samples.getInfo()['features']
-
     data = []
     for feature_set in features:
 
         for k, v in feature_set['properties'].items():
             date, property_name = k[:10], k[11:]
+
             date = date.replace('_', '-')
             data.append({'band': property_name, 'date': date, 'value': v})
 
@@ -61,14 +61,16 @@ def download_map(country_code,
 
     return df
 
-def load_all(country_code='SSD', admin_level='admin1', date_range=('2010-01-01', '2018-01-01'), modis_collection='006/MYD11A2', num_pixels=1000):
+def load_all(country_code='SSD', date_range=('2000-01-01', '2015-01-01'), modis_collection='006/MOD09Q1', num_pixels=1000):
     lower_case_country_code = country_code.lower
     states_shp = f'../raw_data/USA/admin1/USA.shp'
 
-    for state_name in state_names[6:]:
+    for state_name in state_names[0:1]:
 
         list_df = []
-        for start_year in range(2002, 2015):
+        year_lst = [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2017,2018,2019,2020,2021]
+        for start_year in year_lst:
+            print(f"working on {start_year}")
             print(f'working on {state_name}...')
 
             df = download_map(country_code, state_name, (f'{start_year}-01-01', f'{start_year+1}-01-01'), modis_collection, num_pixels)
--- crop_package/models/cnn_model.py
+++ crop_package/models/cnn_model.py
@@ -6,13 +6,16 @@ def cnn():
     model = models.Sequential([
         layers.Normalization(),
         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.BatchNormalization(),
+        layers.Dropout(0.2),
+        layers.LayerNormalization(),
         layers.MaxPooling2D(strides=(2, 2), pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.BatchNormalization(),
+        layers.Dropout(0.2),
+        layers.LayerNormalization(),
         layers.MaxPooling2D(strides=(2, 2), pool_size=(2, 2)),
         layers.Flatten(),
-        layers.BatchNormalization(),
+        layers.LayerNormalization(),
+        layers.Dropout(0.5),
         layers.Dense(1, activation='linear')
     ])
 
