ba79e28d3db322448f114664476d7ef0f75e2ab2
hasib abdulrazaq <hassib12@outlook.com>
2022-09-11 17:46:25 +0100

    added new pickle data files

--- crop_package/download_images_other_countries.py
+++ crop_package/download_images_other_countries.py
@@ -16,7 +16,7 @@ import geemap
 import numpy as np
 import pandas as pd
 
-state_names = pd.read_csv('../raw_data/state_names_USA.csv')['NAME1_,C,33']
+state_names = pd.read_csv('../raw_data/state_names_IND_working_states.csv')['NAME_1,C,22']
 
 # MYD11A2 (2002-2022) 8-day Aqua Land Surface Temperature and Emissivity 1km
 # MOD09A1   surface spectral reflectance of
@@ -24,22 +24,21 @@ state_names = pd.read_csv('../raw_data/state_names_USA.csv')['NAME1_,C,33']
 
 def download_map(country_code,
                  state_name,
-                 date_range=('2021-01-01','2022-12-30'),
-                 modis_collection='006/MOD09A1',
+                 date_range=('2002-01-01','2022-12-30'),
+                 modis_collection='006/MOD11A2',
                  num_pixels=1000):
     Map = geemap.Map()
     lower_case_country_code = country_code.lower
-    states_shp = f'../raw_data/USA/admin1/USA.shp'
+    states_shp = f'../raw_data/IND/admin1/ind.shp'
 
     ee_shape = geemap.shp_to_ee(states_shp)
     state_ee = ee_shape.filter(ee.Filter.eq("NAME1_", state_name))
 
 
     Map.addLayer(state_ee.geometry())
-
+       #  .select(['sur_refl_b01','sur_refl_b02', 'sur_refl_b03', 'sur_refl_b04' ,'sur_refl_b05', 'sur_refl_b06', 'sur_refl_b07'])\
     image_collection = ee.ImageCollection(f'MODIS/{modis_collection}') \
                          .filter(ee.Filter.date(*date_range)) \
-                             .select(['sur_refl_b01','sur_refl_b02', 'sur_refl_b03', 'sur_refl_b04' ,'sur_refl_b05', 'sur_refl_b06', 'sur_refl_b07'])\
                          .toBands()
 
     region = state_ee.geometry()
@@ -64,16 +63,16 @@ def download_map(country_code,
     return df
 
 
-def load_all(country_code='SSD', date_range=('2000-01-01', '2015-01-01'), modis_collection='006/MOD09A1', num_pixels=1000):
+def load_all(country_code='SSD', date_range=('2000-01-01', '2015-01-01'), modis_collection='006/MOD11A2', num_pixels=1000):
     lower_case_country_code = country_code.lower
-    states_shp = f'../raw_data/USA/admin1/USA.shp'
+    states_shp = f'../raw_data/IND/admin1/ind.shp'
                   #working surface refl. layers: [8,9,10,13,15, 16, 17,18,20,22-25,27, 29-30, 32-36,38-43,46,48-49]
                   #working temperature refl. layers: [10 Georgia,16 kansas,25 missouri, 29 nebraska, 36 oklahoma, 41 south dakora, 42 tennessee,43 texas]
-    for state_name in state_names[[10,36,41,42,43,16,25,27]]:
+    for state_name in state_names[0:]:
 
         list_df = []
-        year_lst = [2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2017,2018,2019,2020,2021]
-        for start_year in year_lst:
+        year_lst = [2003,2004,] #2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2017,2018,2019,2020,2021
+        for start_year in range(2003,2013):
             print(f"working on {start_year}")
 
             print(f'working on {state_name}...')
@@ -82,7 +81,7 @@ def load_all(country_code='SSD', date_range=('2000-01-01', '2015-01-01'), modis_
             list_df.append(df)
 
         df = pd.concat(list_df)
-        df.to_pickle(f'../raw_data/raw_pixels/USA/spectral_ref_1000p_scale250_2003-2022-minus2011/{state_name}.zip')
+        df.to_pickle(f'../raw_data/raw_pixels/INDIA/temperature_1000p_scale250_2003-2012/{state_name}.zip')
         print(f"Downloaded data for {state_name}...")
 
 
